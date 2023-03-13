56991aacdd7e45c4770ffb58a0c112d095653723
Mustafabharmal <92981596+Mustafabharmal@users.noreply.github.com>
2023-03-13 22:08:39 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,87 +1,70 @@
 /* eslint-disable no-undef */
-const todoList = require('../todo');
+const todoList = require("../todo");
 
-const {all, markAsComplete, add, dueToday, overdue ,dueLater}= todoList();
+const {
+  all,
+  markAsComplete,
+  add,
+  overdue,
+  dueToday,
+  dueLater,
+  today,
+  tomorrow,
+  yesterday,
+} = todoList();
 
-//var date = new Date();
-const structure_Date = (declare_date) => {
-    return declare_date.toISOString().split("T")[0];
-};
-
-var newDate = new Date();
-// today
-const present_day = structure_Date(newDate);
-
-// yesterDay
-const yesterDay = structure_Date(
-    new Date(new Date().setDate(newDate.getDate() - 1))
-);
-
-//tomorrow
-const nextday = structure_Date(
-    new Date(new Date().setDate(newDate.getDate() + 1))
-);
-
-//test suites
-describe("Todolist Test Case", () => {
-    beforeAll(() => {
-
-        add(
-            {
-                title:"first todo in before block",
-                completed: false,
-                dueDate: present_day
-            },
-        ),
-        add(
-            {
-                
-                title:"Second todo for ",
-                completed: false,
-                dueDate: yesterDay,
-            
-            }
-        ),
-        add(
-            {
-                title:"Third todo for overdue ",
-                completed: false,
-                dueDate:nextday,
-
-            }
-        )
-    })
-    test("Add new todo", () => {  
-        const todoCount = all.length;
-        add(
-            {
-                title: "Add new case  test",
-                completed: true,
-                dueDate:present_day//new Date().toISOString().split("T")[0]
-            }
-        );
-        expect(all.length).toBe(todoCount + 1);
+describe("ToDo test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Submit assignment",
+      dueDate: yesterday,
+      completed: false,
     });
-
-
-    test("mark a todo as completed or not", () => {
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
-    })
-
-//test case for today
-    test("Due2day Testing", () => {
-        expect(dueToday().length).toBe(2);
-    })
-
-//test case for overdue (yesterday)
-    test("overDue testing", () => {
-        expect(overdue().length).toBe(1);
-    })
-    
-//test case for duelater (tomorrow)
-    test("Duelater Testing", () => {
-        expect(dueLater().length).toBe(1);
-    }) 
+    add({
+      title: "Pay rent",
+      dueDate: today,
+      completed: true,
+    });
+  });
+  test("Should add new todo", () => {
+    let count = all.length;
+    add({
+      title: "new todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-IN"),
+    });
+    expect(all.length).toBe(count + 1);
+  });
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("Should Retrieves overdue items", () => {
+    let count = overdue().length;
+    add({
+      title: "Pay electric bill",
+      dueDate: yesterday,
+      completed: false,
+    });
+    expect(overdue().length).toBe(count + 1);
+  });
+  test("Should Retrieves due today items", () => {
+    let count = dueToday().length;
+    add({
+      title: "Pay electric bill",
+      dueDate: today,
+      completed: false,
+    });
+    expect(dueToday().length).toBe(count + 1);
+  });
+  test("Should Retrieves due later items", () => {
+    let count = dueLater().length;
+    add({
+      title: "Pay electric bill",
+      dueDate: tomorrow,
+      completed: false,
+    });
+    expect(dueLater().length).toBe(count + 1);
+  });
 });
